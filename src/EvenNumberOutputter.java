b3da143501a99fe03f7c563fc57e2f2fd8a184fd
Daniel <djplotzke@gmail.com>
2017-10-14 13:07:12 -0700

    Works with all numbers, even odd ones!

--- src/EvenNumberOutputter.java
+++ src/EvenNumberOutputter.java
@@ -2,63 +2,93 @@
  * Daniel Plotzke
  * 10/14/17,
  * 10:41 AM
- * How I tested it:
+ * How I tested it: Used breakpoint for step-by-step debugging, and then testing whole program using all possible
+ * types.
+ * Note: Works with any number! (Not just positive integers!)
  */
 
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class EvenNumberOutputter
 {
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-        double input = -10;
+        System.out.println("Enter any number here!: ");
+        double input = in.nextDouble();
+        double remainderCheck = input % 2;
+        DecimalFormat df = new DecimalFormat("#");
+        if (remainderCheck > 0)
+        {
+            if (input > 0)
+            {
+                System.out.println(df.format(input));
+                input = input - 1;
+            }
+            else
+            {
+                System.out.println(df.format(input));
+                input = input + 1;
+            }
+        }
         double min = (input - 1);
         double max = (input + 1);
-        if (input > 0)
-        {
-            for (input = input; input > min && input < max; )
+            for (; input > min && input < max; )
             {
                 double count = (input / 2);
-
-                if (count >= 0)
+                if (count >= 0 && input > 0)
                 {
-                    System.out.println(input);
-                    input = input - 2;
+                    if (input != 1)
+                    {
+                        {
+                            System.out.println(df.format(input));
+                            input = input - 2;
 
 
-                    min = min - 2;
-                    max = max - 2;
+                            min = min - 2;
+                            max = max - 2;
+                        }
+                    }
+                    else break;
+                }
+                else if (count <= 0 && input < 0)
+                {
+                    if (input != -1)
+                    {
+                        System.out.println(df.format(input));
+                        input = input + 2;
+
+                        min = min + 2;
+                        max = max + 2;
+                    }
+                    else break;
                 }
                 else
                 {
+                    System.out.println(0);
                     break;
                 }
             }
-        }
+//        else if (input < 0)
+//        {
+//            for (input = input; input > min && input < max; )
+//            {
+//                double count = (input /2);
+//
+//                if (count <= 0)
+//                {
+//                    System.out.println(input);
+//                    input = input + 2;
+//
+//                    min = min + 2;
+//                    max = max + 2;
+//                }
 //                else
 //                {
 //                    break;
 //                }
-        else if (input < 0)
-        {
-            for (input = input; input > min && input < max; )
-            {
-                double count = (input /2);
-
-                if (count <= 0)
-                {
-                    System.out.println(input);
-                    input = input + 2;
-
-                    min = min + 2;
-                    max = max + 2;
-                }
-                else
-                {
-                    break;
-                }
-            }
-        }
+//            }
+//        }
     }
 }
